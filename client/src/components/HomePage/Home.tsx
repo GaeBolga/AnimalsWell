20bbdf4c5d86e85e8a268400db05e801118ed10d
GaeBolga <benjamin.lefort@mail.com>
2025-02-12 10:55:03 +0100

    initialisation projet

---
+++ client/src/components/HomePage/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main>
+      <h1>AnimalsWell</h1>
+    </main>
+  );
+}
+
+export default Home;
---
+++ client/src/router/routes.tsx
@@ -0,0 +1,9 @@
+import Home from "../components/HomePage/Home";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
+export default routes;
